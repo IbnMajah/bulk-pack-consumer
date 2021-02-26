489fa9026786306c9e5d342b325afc5fb82decda
Arun Bakt <arunbakt@gmail.com>
2021-02-26 13:52:42 -0800

    fix: rename sqs url env variable name

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 exports.connectionString = process.env.DATABASE_URL
-exports.queueUrl = process.env.SQS_QUEUE_URL
+exports.queueUrl = process.env.SQS_URL
 exports.awsconfig = { 
     "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
     "secretAccessKey": process.env.AWS_ACCESS_KEY,
