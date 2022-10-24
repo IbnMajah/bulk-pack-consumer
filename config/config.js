68ed0018f5c0c6e0345dfef7a47140663d45a28b
MagnificentTerminator <majaboujallow@gmail.com>
2022-10-24 20:42:28 +0000

    feat: set-up for local

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
-exports.connectionString = process.env.DATABASE_URL
-exports.queueUrl = process.env.SQS_URL
-exports.awsconfig = { 
-    "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
-    "secretAccessKey": process.env.AWS_ACCESS_KEY,
-    "region" : "eu-central-1",
-    "output" : "json"
-}
+exports.connectionString = process.env.DATABASE_URL;
+exports.queueUrl = process.env.SQS_URL;
+exports.awsconfig = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "us-east-1",
+  output: "json",
+};
--- consumer.js
+++ consumer.js
@@ -1,93 +1,89 @@
-require('dotenv').config();
-const config = require('./config/config')
+require("dotenv").config();
+const config = require("./config/config");
 
-const { Consumer } = require('sqs-consumer');
-var aws = require('aws-sdk');
+const { Consumer } = require("sqs-consumer");
+var aws = require("aws-sdk");
 aws.config.update({ ...config.awsconfig });
 const s3 = new aws.S3();
 const Region = aws.config.region;
 const QueueUrl = config.queueUrl;
 
-const { Pool, Client } = require('pg');
+const { Pool, Client } = require("pg");
+const { S3 } = require("aws-sdk");
 const pool = new Pool({ connectionString: config.connectionString });
 
 //const Sentry = require('@sentry/node');
 //Sentry.init({ dsn: config.sentryDSN });
 
-console.log(Region)
-console.log(QueueUrl)
+console.log(Region);
+console.log(QueueUrl);
+// console.log(S3);
 const app = Consumer.create({
-    queueUrl: QueueUrl,
-    region: Region,
-    batchSize: 10, 
-    handleMessage: async function(message) {
+  queueUrl: QueueUrl,
+  region: Region,
+  batchSize: 10,
+  handleMessage: async function (message) {
+    console.log(message.Body);
+    var body = JSON.parse(message.Body);
+    if (body.Event == "s3:TestEvent") {
+      console.log("Test Event");
+      return;
+    }
 
-      var body = JSON.parse(message.Body);
-      console.log(body);
-      if(body.Event == 's3:TestEvent'){
-        console.log('Test Event');
-        return;
-      }
+    var records = body["Records"];
+    console.log(records);
 
-      var records = body['Records'];
-      //console.log(records);
+    for (let record of records) {
+      // console.log(record);
+      var fullQueueRecord = JSON.stringify(record);
+      console.log("queue record: ", fullQueueRecord);
+      const eventTime = record["eventTime"];
+      const bucketArn = record["s3"]["bucket"]["arn"];
+      const bucketName = record["s3"]["bucket"]["name"];
+      const key = record["s3"]["object"]["key"];
 
-      for(let record of records) {
-        //console.log(record);
-        var fullQueueRecord = JSON.stringify(record);
-        console.log(fullQueueRecord);
-        const eventTime = record['eventTime'];
-        const bucketArn = record['s3']['bucket']['arn'];
-        const bucketName = record['s3']['bucket']['name'];
-        const key = record['s3']['object']['key'];
+      const bulkData = await fetchS3Data(key, bucketName);
 
-        const bulkData = await fetchS3Data(key, bucketName);
+      // TODO put this into a table with the above pieces
 
-        // TODO put this into a table with the above pieces                                          
-
-        const insert = {
-          text: `INSERT INTO bulk_tree_upload
+      const insert = {
+        text: `INSERT INTO bulk_tree_upload
           (queue_record, event_time, bucket_arn, key, bulk_data)
           values
           ($1, $2, $3, $4, $5)`,
-          values: [ fullQueueRecord, eventTime, bucketArn, key, bulkData ]
-        };
-        console.log(insert);
-        await pool.query(insert);
-
-
-      }
-
-    }   
+        values: [fullQueueRecord, eventTime, bucketArn, key, bulkData],
+      };
+      console.log(insert);
+      await pool.query(insert);
+    }
+  },
 });
 
-app.on('error', function (err) {
-    console.log('error', err);
+app.on("error", function (err) {
+  console.log("error", err);
 });
 
-app.on('processing_error', function (err) {
-    console.log('processing_error', err);
+app.on("processing_error", function (err) {
+  console.log("processing_error", err);
 });
 
 async function fetchS3Data(key, bucket) {
-    params = {
-        Bucket: bucket,
-        Key: key
-    };
+  params = {
+    Bucket: bucket,
+    Key: key,
+  };
 
-    const s3GetObjectPromise = new Promise((resolve, reject) => {
-        s3.getObject(params, (err, data) => {
-            if ( err ) reject(err)
-            else resolve(data)
-        })
+  const s3GetObjectPromise = new Promise((resolve, reject) => {
+    s3.getObject(params, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
     });
+  });
 
-
-    const data = await s3GetObjectPromise;
-    let objectData = JSON.parse(data.Body.toString('utf-8'));
-    return objectData;
+  const data = await s3GetObjectPromise;
+  let objectData = JSON.parse(data.Body.toString("utf-8"));
+  console.log("Buld data: ", objectData);
+  return objectData;
 }
 
-
-app.start()
-
+app.start();
